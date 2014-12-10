08471db55618137413deb5ade0a9e16e900dccb8
kavoelker <kavoelker@users.noreply.github.com>
2014-12-10 15:03:29 -0800

    Update mis407finalproject.py

--- mis407finalproject.py
+++ mis407finalproject.py
@@ -1,17 +1,21 @@
 import pandas as pd
 import json
 import urllib2
+import csv
+
+results = ''
 
 
 
 def loadCraigslistData():
 	#load csv data from api created in kimonolabs that grabs craigslist data
 	print("Collecting Craigslist Data For Electronics in Ames, Iowa")
-	request = urllib2.Request("https://www.kimonolabs.com/api/b0c7flky?apikey=qvdiZcoVwcx9TlWbDGcqdWMtSamsK7AB", headers={"authorization" : "Bearer mi6aboJU5bcLWzPDuGevrseP5mkMQ9Wt"})
+	request = urllib2.Request("https://www.kimonolabs.com/api/csv/b0c7flky?apikey=qvdiZcoVwcx9TlWbDGcqdWMtSamsK7AB", headers={"authorization" : "Bearer mi6aboJU5bcLWzPDuGevrseP5mkMQ9Wt"})
 	contents = urllib2.urlopen(request).read()
-	dataResults = json.loads(contents)
-	print type(dataResults)
-	print dataResults['collection1']["property3"]
+	
+	url = "clist.csv"
+	results = pd.read_csv(url)
+	print results
 
 #def chooseAction():
 	#ask user what action they would like to perform, search/sort
@@ -23,5 +27,7 @@ def loadCraigslistData():
 	#search csv for items based on keywords provided by user
 	#store list temporarily to give user choice to sort by price
 	#show user what percentage of items match their keyword
+	queryResult = results.query('property3 == "$50"')
+	print(queryResult)
 
 loadCraigslistData()
